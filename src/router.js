b9807fc993bf9f8e439e14d223458bebc09bd4fe
SuChuang <cceecc@sina.cn>
2016-08-04 11:53:14 +0800

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,18 @@
+/**
+ * Created with WebStrom 10.0
+ * User：苏国均
+ * Version：1.0.0
+ * Date：16/8/4
+ * Time：
+ * Description：
+ */
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+
+module.exports = app;
---
+++ src/routers/translate.js
@@ -0,0 +1,45 @@
+/**
+ * Created with WebStorm 10.0
+ * User：苏国均
+ * Version：1.0.0
+ * Date：16/8/4
+ * Time：
+ * Description：
+ */
+
+var router = require('express').Router();
+var AV = require('leanengine');
+
+var TranslateRecord = AV.Object.extend('TranslateRecord');
+
+
+router.post('/addTranslateRecord', function (req, res, next) {
+    var word = req.params.word;
+    var translate = req.params.translate;
+    if (!word || !translate) {
+        res.send({code: 0, info: 'params error!!'});
+    }
+    var translateRecord = new TranslateRecord();
+    translateRecord.set('word', word);
+    translateRecord.set('translate', translate);
+    translateRecord.save().then(function (translateRecord) {
+        res.send({code: 1, info: translateRecord});
+    }, function (error) {
+        console.log("error->" + error);
+        res.send({code: 0, info: error});
+    });
+});
+
+
+router.get('/TranslateRecord', function (req, res, next) {
+    var query = new AV.Query(TranslateRecord);
+    query.find().then(function (translateRecord) {
+        res.send({code: 1, info: translateRecord});
+    }, function (error) {
+        console.log("error->" + error);
+        res.send({code: 0, info: error});
+    });
+});
+
+
+module.exports = router;
